3c9aa6d0d65acee75273b63cbfbebee3e4d9243a
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-05-20 19:55:59 +0200

    fix: update function visibility in get_next_line implementation

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 18:07:11 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/05/20 16:34:16 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/05/20 19:49:43 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,5 @@
 
 char	*get_next_line(int fd);
 char	*strjoin_till_nl(char *line, char *buffer);
-void	shift_buffer_after_nl(char *buffer);
-char	*strdup_till_nl(char *str);
-char	*read_and_join(int fd, char *buffers, char *line);
-char	*handle_empty_line(char *buffers, char *line);
 
 #endif
--- src/get_next_line/get_next_line.c
+++ src/get_next_line/get_next_line.c
@@ -6,13 +6,98 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 19:21:57 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/01/13 14:58:47 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/05/20 19:50:48 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdlib.h>
 #include <unistd.h>
+
+static char	*strdup_till_nl(char *str);
+static char	*read_and_join(int fd, char *buffers, char *line);
+static char	*handle_empty_line(char *buffers, char *line);
+static void	shift_buffer_after_nl(char *buffer);
+
+static char	*strdup_till_nl(char *str)
+{
+	char	*dup;
+	size_t	len;
+	size_t	i;
+
+	len = 0;
+	i = 0;
+	while (str[len] && str[len] != '\n')
+		len++;
+	if (str[len] == '\n')
+		len++;
+	dup = malloc(len + 1);
+	if (!dup)
+		return (NULL);
+	while (i < len)
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+static char	*read_and_join(int fd, char *buffers, char *line)
+{
+	int	bytes;
+
+	bytes = 1;
+	while (bytes > 0 && !ft_strchr(line, '\n'))
+	{
+		bytes = read(fd, buffers, BUFFER_SIZE);
+		if (bytes == -1)
+		{
+			buffers[0] = '\0';
+			free(line);
+			return (NULL);
+		}
+		buffers[bytes] = '\0';
+		line = strjoin_till_nl(line, buffers);
+		if (!line)
+		{
+			buffers[0] = '\0';
+			return (NULL);
+		}
+	}
+	return (line);
+}
+
+static char	*handle_empty_line(char *buffers, char *line)
+{
+	if (!line[0])
+	{
+		buffers[0] = '\0';
+		free(line);
+		return (NULL);
+	}
+	return (line);
+}
+
+static void	shift_buffer_after_nl(char *buffer)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\n')
+		i++;
+	while (buffer[i])
+	{
+		buffer[j] = buffer[i];
+		i++;
+		j++;
+	}
+	buffer[j] = '\0';
+}
 
 char	*get_next_line(int fd)
 {
@@ -41,84 +126,4 @@ char	*get_next_line(int fd)
 	}
 	shift_buffer_after_nl(buffers[fd]);
 	return (line);
-}
-
-char	*strdup_till_nl(char *str)
-{
-	char	*dup;
-	size_t	len;
-	size_t	i;
-
-	len = 0;
-	i = 0;
-	while (str[len] && str[len] != '\n')
-		len++;
-	if (str[len] == '\n')
-		len++;
-	dup = malloc(len + 1);
-	if (!dup)
-		return (NULL);
-	while (i < len)
-	{
-		dup[i] = str[i];
-		i++;
-	}
-	dup[i] = '\0';
-	return (dup);
-}
-
-char	*read_and_join(int fd, char *buffers, char *line)
-{
-	int	bytes;
-
-	bytes = 1;
-	while (bytes > 0 && !ft_strchr(line, '\n'))
-	{
-		bytes = read(fd, buffers, BUFFER_SIZE);
-		if (bytes == -1)
-		{
-			buffers[0] = '\0';
-			free(line);
-			return (NULL);
-		}
-		buffers[bytes] = '\0';
-		line = strjoin_till_nl(line, buffers);
-		if (!line)
-		{
-			buffers[0] = '\0';
-			return (NULL);
-		}
-	}
-	return (line);
-}
-
-char	*handle_empty_line(char *buffers, char *line)
-{
-	if (!line[0])
-	{
-		buffers[0] = '\0';
-		free(line);
-		return (NULL);
-	}
-	return (line);
-}
-
-void	shift_buffer_after_nl(char *buffer)
-{
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	j = 0;
-	while (buffer[i] && buffer[i] != '\n')
-		i++;
-	if (buffer[i] == '\n')
-		i++;
-	while (buffer[i])
-	{
-		buffer[j] = buffer[i];
-		i++;
-		j++;
-	}
-	buffer[j] = '\0';
 }
