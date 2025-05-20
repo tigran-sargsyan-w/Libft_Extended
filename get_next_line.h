63e7140b817afd98af0bc1f9ab1735f1aac61440
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-05-20 16:52:37 +0200

    fix: update header files and remove unused functions in get_next_line (multiple-definition)

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 18:07:11 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/04/27 13:35:38 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/05/20 16:34:16 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,9 @@
 
 char	*get_next_line(int fd);
 char	*strjoin_till_nl(char *line, char *buffer);
-char	*ft_strchr(const char *s, int c);
 void	shift_buffer_after_nl(char *buffer);
 char	*strdup_till_nl(char *str);
 char	*read_and_join(int fd, char *buffers, char *line);
 char	*handle_empty_line(char *buffers, char *line);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif
--- src/get_next_line/get_next_line_utils.c
+++ src/get_next_line/get_next_line_utils.c
@@ -6,25 +6,12 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 18:07:00 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/04/27 13:35:28 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/05/20 16:30:58 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdlib.h>
-
-char	*ft_strchr(const char *s, int c)
-{
-	while (*s)
-	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
-	}
-	if ((char)c == 0)
-		return ((char *)s);
-	return (NULL);
-}
 
 char	*strjoin_till_nl(char *line, char *buffer)
 {
@@ -53,19 +40,4 @@ char	*strjoin_till_nl(char *line, char *buffer)
 		str[j++] = buffer[i++];
 	str[j] = '\0';
 	return (free(line), str);
-}
-
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	size_t	i;
-
-	if (dest == NULL && src == NULL)
-		return (dest);
-	i = 0;
-	while (i < n)
-	{
-		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
-		i++;
-	}
-	return (dest);
 }
