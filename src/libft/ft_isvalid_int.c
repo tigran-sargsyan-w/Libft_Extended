f6d7056003ce69a5d4aef10b51bd984b4e7e1bc4
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-05-20 16:48:56 +0200

    fix: refactor ft_isvalid_int to improve overflow checking logic

--- src/libft/ft_isvalid_int.c
+++ src/libft/ft_isvalid_int.c
@@ -6,25 +6,46 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 11:00:00 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/03/18 11:16:51 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/05/20 14:22:34 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
 
-int	ft_isvalid_int(const char *str)
+static int	check_overflow_digits(const char *str, int sign)
 {
-	while (*str && ft_isspace(*str))
-		str++;
-	if (*str == '-' || *str == '+')
-		str++;
-	if (!ft_isdigit(*str))
-		return (0);
+	long	result;
+
+	result = 0;
 	while (*str && !ft_isspace(*str) && *str != ',')
 	{
 		if (!ft_isdigit(*str))
 			return (0);
+		result = result * 10 + (*str - '0');
+		if (sign == 1 && result > INT_MAX)
+			return (0);
+		if (sign == -1 && - result < INT_MIN)
+			return (0);
 		str++;
 	}
 	return (1);
+}
+
+int	ft_isvalid_int(const char *str)
+{
+	int	sign;
+
+	sign = 1;
+	while (*str && ft_isspace(*str))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	if (!ft_isdigit(*str))
+		return (0);
+	return (check_overflow_digits(str, sign));
 }
