2294402e79def44779eef01e15a4bde54d97418a
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-11-01 00:04:35 +0100

    refactor: improve variable naming and code readability in ft_strtod function

--- src/libft/ft_strtod.c
+++ src/libft/ft_strtod.c
@@ -6,114 +6,112 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 23:10:42 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/10/31 23:22:27 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/11/01 00:03:13 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static double	ft_pow10(int exp)
+static double	ft_pow10(int exponent)
 {
-	double	res;
+	double	result;
 
-	res = 1.0;
-	if (exp > 0)
+	result = 1.0;
+	if (exponent > 0)
 	{
-		while (exp-- > 0)
-			res *= 10.0;
+		while (exponent-- > 0)
+			result *= 10.0;
 	}
-	else if (exp < 0)
+	else if (exponent < 0)
 	{
-		while (exp++ < 0)
-			res /= 10.0;
+		while (exponent++ < 0)
+			result /= 10.0;
 	}
-	return (res);
+	return (result);
 }
 
-static const char	*read_digits(
-	const char *p,
-	long double *m,
-	int *cnt,
-	int *has)
+static const char	*read_digits(const char *str_pointer,
+								long double *mantissa,
+								int *count, int *has_digits)
 {
-	while (*p && ft_isdigit((unsigned char)*p))
+	while (*str_pointer && ft_isdigit((unsigned char)*str_pointer))
 	{
-		*m = (*m * 10.0L) + (long double)(*p - '0');
-		p++;
-		if (cnt)
-			(*cnt)++;
-		*has = 1;
+		*mantissa = (*mantissa * 10.0L) + (long double)(*str_pointer - '0');
+		str_pointer++;
+		if (count)
+			(*count)++;
+		*has_digits = 1;
 	}
-	return (p);
+	return (str_pointer);
 }
 
-static const char	*read_exp(const char *p, int has_digits, int *exp10)
+static const char	*read_exp(const char *pointer, int has_digits, int *exp10)
 {
 	const char	*save;
-	int			expsign;
-	int			expval;
+	int			exponent_sign;
+	int			exponent_value;
 
-	if (!(has_digits && (*p == 'e' || *p == 'E')))
-		return (p);
-	save = p;
-	p++;
-	expsign = 1;
-	if (*p == '+' || *p == '-')
+	if (!(has_digits && (*pointer == 'e' || *pointer == 'E')))
+		return (pointer);
+	save = pointer;
+	pointer++;
+	exponent_sign = 1;
+	if (*pointer == '+' || *pointer == '-')
 	{
-		if (*p == '-')
-			expsign = -1;
-		p++;
+		if (*pointer == '-')
+			exponent_sign = -1;
+		pointer++;
 	}
-	expval = 0;
-	if (!ft_isdigit((unsigned char)*p))
+	exponent_value = 0;
+	if (!ft_isdigit((unsigned char)*pointer))
 		return (save);
-	while (*p && ft_isdigit((unsigned char)*p))
+	while (*pointer && ft_isdigit((unsigned char)*pointer))
 	{
-		expval = (expval * 10) + (*p - '0');
-		p++;
+		exponent_value = (exponent_value * 10) + (*pointer - '0');
+		pointer++;
 	}
-	*exp10 += expsign * expval;
-	return (p);
+	*exp10 += exponent_sign * exponent_value;
+	return (pointer);
 }
 
-static const char	*read_ws_sign(const char *p, int *sign)
+static const char	*read_ws_sign(const char *pointer, int *sign)
 {
-	while (*p && ft_isspace((unsigned char)*p))
-		p++;
+	while (*pointer && ft_isspace((unsigned char)*pointer))
+		pointer++;
 	*sign = 1;
-	if (*p == '+' || *p == '-')
+	if (*pointer == '+' || *pointer == '-')
 	{
-		if (*p == '-')
+		if (*pointer == '-')
 			*sign = -1;
-		p++;
+		pointer++;
 	}
-	return (p);
+	return (pointer);
 }
 
 double	ft_strtod(const char *nptr, char **endptr)
 {
-	const char				*p;
-	int						sign;
-	t_strtod_state			s;
+	const char		*pointer;
+	int				sign;
+	t_strtod_state	state;
 
-	p = read_ws_sign(nptr, &sign);
-	s.m = 0.0L;
-	s.frac = 0;
-	s.has = 0;
-	p = read_digits(p, &s.m, NULL, &s.has);
-	if (*p == '.')
+	pointer = read_ws_sign(nptr, &sign);
+	state.m = 0.0L;
+	state.frac = 0;
+	state.has = 0;
+	pointer = read_digits(pointer, &state.m, NULL, &state.has);
+	if (*pointer == '.')
 	{
-		p = read_digits(p + 1, &s.m, &s.frac, &s.has);
+		pointer = read_digits(pointer + 1, &state.m, &state.frac, &state.has);
 	}
-	s.exp10 = -s.frac;
-	p = read_exp(p, s.has, &s.exp10);
-	if (!s.has)
+	state.exp10 = -state.frac;
+	pointer = read_exp(pointer, state.has, &state.exp10);
+	if (!state.has)
 	{
 		if (endptr)
 			*endptr = (char *)nptr;
 		return (0.0);
 	}
 	if (endptr)
-		*endptr = (char *)p;
-	return ((double)(sign * s.m * (long double)ft_pow10(s.exp10)));
+		*endptr = (char *)pointer;
+	return ((double)(sign * state.m * (long double)ft_pow10(state.exp10)));
 }
